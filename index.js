c052a82c9f3e0e0a194e8cd5ec47ee0d50be16f6
MdShijanAli <freelancershijan@gmail.com>
2023-12-22 22:22:54 +0600

    Server setup and mongodb connect

---
+++ index.js
@@ -0,0 +1,65 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jqheb6c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+
+  try {
+      const productsCollection = client.db("productTaskDB").collection("products");
+ 
+
+    
+     // get all Products
+
+
+     app.get('/api/products', async (req, res) => {
+      const query = {};
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+  })
+
+    
+
+
+      
+
+  }
+  finally {
+
+  }
+
+
+}
+run().catch(err => console.log(err));
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Server is Running')
+})
+
+app.listen(port, () => {
+  console.log(`Server is Running on PORT: ${port}`)
+})
